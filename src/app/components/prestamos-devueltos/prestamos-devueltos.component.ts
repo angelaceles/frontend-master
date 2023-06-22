2753e3d4b6c98108b7725d4b8a1406548194e007
angelaceles <angelaceleste098@gmail.com>
2023-06-22 13:50:23 -0500

    Secont commit

---
+++ src/app/components/prestamos-devueltos/prestamos-devueltos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prestamos-devueltos',
+  templateUrl: './prestamos-devueltos.component.html',
+  styleUrls: ['./prestamos-devueltos.component.css']
+})
+export class PrestamosDevueltosComponent {
+
+}
---
+++ src/app/components/prestamos-done/prestamos-done.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prestamos-done',
+  templateUrl: './prestamos-done.component.html',
+  styleUrls: ['./prestamos-done.component.css']
+})
+export class PrestamosDoneComponent {
+
+}
